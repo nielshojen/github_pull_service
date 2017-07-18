0c3fe2b328a4cd035e56e19145a261e7d36efe1e
Niels Højen <niels@hojen.net>
2017-07-18 22:56:25 +0200

    Update git_pull_status.sh

--- git_pull_status.sh
+++ git_pull_status.sh
@@ -4,11 +4,11 @@ source git_pull_settings.sh
 
 if [ ! -d "$PULLDIR/.git" ]; then
   echo "Not pulled yet"
-  STATUS="2"
+  STATUS="255"
 else
   USEDBRANCH=$(git -C $PULLDIR status | grep 'On branch' | awk -F ' branch ' '{ print $2 }')
-  STATUS=$(git -C $PULLDIR status | grep 'Your branch is up-to-date' | wc -l | xargs)
-  if [ "$USEDBRANCH" = "$BRANCH" ] && [ "$STATUS" = "1" ]; then
+  STATUS=$(git -C $PULLDIR pull --dry-run | wc -l | xargs)
+  if [ "$USEDBRANCH" = "$BRANCH" ] && [ "$STATUS" = "0" ]; then
     echo "Already up-to-date"
     #exit 1
   fi
