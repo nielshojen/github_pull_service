1ee9cbb298e25ff69b9931fc182e2eb251f72e8b
Niels Højen <niels@hojen.net>
2017-07-18 23:22:57 +0200

    Update git_pull_status.sh

--- git_pull_status.sh
+++ git_pull_status.sh
@@ -2,15 +2,15 @@
 
 source git_pull_settings.sh
 
-UPSTREAM=${1:-'@{u}'}
-LOCAL=$(git -C $PULLDIR rev-parse @)
-REMOTE=$(git -C $PULLDIR rev-parse "$UPSTREAM")
-BASE=$(git -C $PULLDIR merge-base @ "$UPSTREAM")
-
 if [ ! -d "$PULLDIR/.git" ]; then
   echo "Not cloned yet"
   STATUS="2"
 else
+  git -C /tmp/testrepo remote update
+  UPSTREAM=${1:-'@{u}'}
+  LOCAL=$(git -C $PULLDIR rev-parse @)
+  REMOTE=$(git -C $PULLDIR rev-parse "$UPSTREAM")
+  BASE=$(git -C $PULLDIR merge-base @ "$UPSTREAM")
   if [ $LOCAL = $REMOTE ]; then
     echo "Already Up-to-date"
     STATUS="0"
