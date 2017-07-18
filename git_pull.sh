31863d6a1ad21c910ca8056474beb8bd0e80ec6e
Niels Højen <niels@hojen.net>
2017-07-18 23:04:42 +0200

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -4,11 +4,11 @@ source git_pull_settings.sh
 source git_pull_prereqs.sh
 source git_pull_status.sh
 
-if [ "$STATUS" = "255" ]; then
+if [ "$STATUS" = "1" ]; then
   echo "Cloning $GITREPOURL to $CLONEDIR"
   mkdir -p $CLONEDIR
   git -C $CLONEDIR clone $GITREPOURL
-elif [ "$STATUS" -gt "0" ]; then
+elif [ "$STATUS" = "1" ]; then
   echo "Pulling $GITREPOURL to $PULLDIR"
   git -C $PULLDIR pull
 fi
