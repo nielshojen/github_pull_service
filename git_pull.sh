682244b585fcfc2ecdee366818d658393312c9e4
Niels Højen <niels@hojen.net>
2017-07-18 22:17:58 +0200

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -5,8 +5,10 @@ source git_pull_prereqs.sh
 source git_pull_status.sh
 
 if [ "$STATUS" = "2" ]; then
+  echo "Cloning to $PULLDIR"
   git -C $PULLDIR clone
 elif [ "$STATUS" = "0" ]; then
+  echo "Pulling to $PULLDIR"
   git -C $PULLDIR pull
 fi
 
