0a22b0a26c839aa4be06e3964aaeeaf830c48412
Niels Højen <niels@hojen.net>
2017-07-18 22:47:36 +0200

    Update git_pull_settings.sh

--- git_pull_settings.sh
+++ git_pull_settings.sh
@@ -2,7 +2,7 @@
 
 GITUSER="[EditMe]"
 GITREPO="[EditMe]"
-GITREPOURL="https://github.com/$GITUSER/$REPO.git"
+GITREPOURL="https://github.com/$GITUSER/$GITREPO.git"
 CLONEDIR="[EditMe]"
 PULLDIR="$CLONEDIR/$GITREPO"
 BRANCH="master"
