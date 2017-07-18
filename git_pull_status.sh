8ab8bce6e96f09b9c3c2fffc22f8488790d7161b
Niels Højen <niels@hojen.net>
2017-07-18 19:18:17 +0200

    Update git_pull_status.sh

--- git_pull_status.sh
+++ git_pull_status.sh
@@ -2,8 +2,8 @@
 
 source git_pull_settings.sh
 
-USEDBRANCH=$(git status | grep 'On branch' | awk -F ' branch ' '{ print $2 }')
-STATUS=$(git status | grep 'Your branch is up-to-date' | wc -l | xargs)
+USEDBRANCH=$(git -C $PULLDIR status | grep 'On branch' | awk -F ' branch ' '{ print $2 }')
+STATUS=$(git -C $PULLDIR status | grep 'Your branch is up-to-date' | wc -l | xargs)
 
 if [ "$USEDBRANCH" = "$BRANCH" ] && [ "$STATUS" = "1" ]; then
   exit 0
