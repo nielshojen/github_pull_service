0c370ba0a6c0b17db4c10737c6803f80539a7491
Niels Højen <niels@hojen.net>
2017-07-18 22:30:57 +0200

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -7,7 +7,7 @@ source git_pull_status.sh
 if [ "$STATUS" = "2" ]; then
   echo "Cloning to $PULLDIR"
   mkdir -p $PULLDIR
-  git -C $PULLDIR clone
+  git -C $PULLDIR clone $GITREPO
 elif [ "$STATUS" = "0" ]; then
   echo "Pulling to $PULLDIR"
   git -C $PULLDIR pull
